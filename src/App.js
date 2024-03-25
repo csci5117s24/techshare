11e313bdf9ea41d0d785d39aaa122013fb4cbd47
HEMANTH KUMAR TIRUPATI <tirupatihemanthkumar@gmail.com>
2024-03-25 15:02:27 -0500

    Add Item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import Item from './components/Item.jsx'
 import data from './mock.js'
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [quant, setQuant] = useState("");
+  const [expDate, setExpDate]= useState("");
+
+  const [itemData, setItemData] = useState(data);
+  
+  function newData(){
+    let arrayLen = itemData.length;
+    const newItem = {id:arrayLen+1, name:name, quantity:quant, expireDate:expDate};
+    setItemData([...itemData, newItem]);
+  }
+
   return (
     <>
       <h1>Fridge Tracker</h1>
-      {data.map(datum => <Item key={datum.id} datum={datum} />)}
+      {itemData.map(datum => <Item key={datum.id} datum={datum} />)}
+
+
+      <div>
+          <input value={name} onChange={e=>setName(e.target.value)} placeholder="Name"></input>
+          <input value={quant} onChange={e=>setQuant(e.target.value)} placeholder="Quantity"></input>
+          <input value={expDate} onChange={e=>setExpDate(e.target.value)} placeholder="Expiry: MM/DD/YYYY"></input>
+          <button onClick={newData}>Add Item</button>
+      </div>
     </>
   );
 }
